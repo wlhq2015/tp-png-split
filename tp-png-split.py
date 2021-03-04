67201cff2f87ed7aa1d1a7bd33480b648ffb8f03
wlhq2015 <1337177500@qq.com>
2021-03-04 17:13:03 +0800

    Update tp-png-split.py
    more support

--- tp-png-split.py
+++ tp-png-split.py
@@ -1,19 +1,30 @@
+
+
 import plistlib
 import os
+import os.path
 import sys
 from PIL import Image
 
 
 def export_image(img, pathname, item):
     # 去透明后的子图矩形
-    x, y, w, h = tuple(map(int, item['frame']))
+    x, y, w, h = tuple(map(int, item['textureRect']))
     # 子图原始大小
-    size = tuple(map(int, item['sourceSize']))
+    size = tuple(map(int, item['spriteSourceSize']))
     # 子图在原始图片中的偏移
-    ox, oy, _, _ = tuple(map(int, item['sourceColorRect']))
+    if(item['spriteOffset'][0].find('.') > 0 or item['spriteOffset'][1].find('.') > 0):
+        print(">>>>>>>>>>>not save>>>>>>>>>>>>")
+        print(pathname, item['spriteOffset'][0],item['spriteOffset'][1])
+        print("<<<<<<<<<<<<<<<not sav<<<<<<<<<<<")
+        return
+    ox, oy = tuple(map(int, item['spriteOffset']))
+
+
+    
 
     # 获取子图左上角，右下角
-    if item['rotated']:
+    if item['textureRotated']:
         box = (x, y, x + h, y + w)
     else:
         box = (x, y, x + w, y + h)
@@ -24,7 +35,7 @@ def export_image(img, pathname, item):
     sprite = img.crop(box)
 
     # rotated纹理旋转90度
-    if item['rotated']:
+    if item['textureRotated']:
         sprite = sprite.transpose(Image.ROTATE_90)
 
     # 粘贴子图，设置偏移
@@ -32,16 +43,27 @@ def export_image(img, pathname, item):
 
     # 保存到文件
     print('保存文件：%s' % pathname)
+
+    pathdir = os.path.split(pathname)[0]
+    if(not os.path.exists(pathdir)):
+        os.makedirs(pathdir)
+
     image.save(pathname, 'png')
 
 # 获取 frame 参数
 def get_frame(frame):
     result = {}
-    if frame['frame']:
-        result['frame'] = frame['frame'].replace('}', '').replace('{', '').split(',')
-        result['sourceSize'] = frame['sourceSize'].replace('}', '').replace('{', '').split(',')
-        result['sourceColorRect'] = frame['sourceColorRect'].replace('}', '').replace('{', '').split(',')
-        result['rotated'] = frame['rotated']
+    if 'textureRect' in frame:
+
+        result['textureRect'] = frame['textureRect'].replace('}', '').replace('{', '').split(',')
+        result['spriteSourceSize'] = frame['spriteSourceSize'].replace('}', '').replace('{', '').split(',')
+        result['spriteOffset'] = frame['spriteOffset'].replace('}', '').replace('{', '').split(',')
+        result['textureRotated'] = frame['textureRotated']
+    if 'frame' in frame:
+        result['textureRect'] = frame['frame'].replace('}', '').replace('{', '').split(',')
+        result['spriteSourceSize'] = frame['sourceSize'].replace('}', '').replace('{', '').split(',')
+        result['spriteOffset'] = frame['offset'].replace('}', '').replace('{', '').split(',')
+        result['textureRotated'] = frame['rotated']
     return result
 
 # 生成图片
@@ -71,15 +93,42 @@ def gen_image(file_name, export_path):
     img = Image.open(file_name + '.png')
 
     # 读取所有小图数据
+    if not 'frames' in lp:
+        print(">>>>>>>>>>>>>>>>>>>>>>>")
+        print(file_name)
+        print("<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        return
+
     frames = lp['frames']
+    # print(frames)
     for key in frames:
+        # print(key)
+        # print(frames[key])
         item = get_frame(frames[key])
         export_image(img, os.path.join(export_path, key), item)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    if len(sys.argv) == 3:
-        filename = sys.argv[1]
-        exportPath = sys.argv[2]
-        gen_image(filename, exportPath)
+# gen_image("fireball_sub_explosion", "fireball_sub_explosion")
+
+
+# 遍历文件夹
+def walkFile(file):
+    print("---------------")
+    for root, dirs, files in os.walk(file):
+
+        # root 表示当前正在访问的文件夹路径
+        # dirs 表示该文件夹下的子目录名list
+        # files 表示该文件夹下的文件list
+
+        # 遍历文件
+        for f in files:
+            imgPath = os.path.join(root, f)
+            if f.endswith('.plist'):
+                imgPath = os.path.join(root, f)
+                file_name = os.path.splitext(imgPath)[0]
+                gen_image(file_name,file_name)
+
+                
+
+            
+walkFile(".")
